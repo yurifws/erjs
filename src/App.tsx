e6dbb2ccba957fa4a85a095e639513f9afb44494
yurifws <yurifws@gmail.com>
2022-01-07 01:03:09 -0300

    Criando o componente de Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HelloWorld />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Olá mundo!

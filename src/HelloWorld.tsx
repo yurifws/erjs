51db0da1972ca2deefdd1da5bb731e06143167bb
yurifws <yurifws@gmail.com>
2022-01-07 01:46:33 -0300

    Atributos com JSX

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,9 +1,10 @@
 export default function HelloWorld() {
   const name = 'Daniel'
   const title = <h1>Hello, { name }!!!</h1>
+  const index = 0
 
   if(name === 'Daniel'){
-    return <h1 style={{background: 'red'}}>{name}</h1>
+    return <h1 tabIndex={index} style={{background: 'red'}}>{name}</h1>
   }
 
   return <div>

6770f88c9a6008c4862f97ef49cd699768330968
yurifws <yurifws@gmail.com>
2022-01-07 01:36:19 -0300

    Incorporando expressões dentro do JSX

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,5 +1,8 @@
 export default function HelloWorld() {
+  const name = 'Daniel'
+
   return <div>
-    <h1>Hello World!!!</h1>
+    <h1>Hello, { name === 'Daniel' ? 'Daniel': name}!!!</h1>
+    <h1>Hello, { name }!!!</h1>
   </div>
 }

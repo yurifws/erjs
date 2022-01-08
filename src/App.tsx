bf1f333cf40532e5c53b77dec6e9f7e19dc126c3
yurifws <yurifws@gmail.com>
2022-01-08 00:50:29 -0300

    Atualizando um componente em tempo real

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-const Componet = React.createElement('div', {style: {background: 'red'}}, 'Olá mundo')
-
-const HTMLElement = document.createElement('div')
-HTMLElement.style.background = 'red'
-HTMLElement.innerText = 'OLÁ MUNDO'
-
-console.log(Componet)
-console.dir(HTMLElement)
+import Clock from './Clock';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {Componet }
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock/>
+        
       </header>
     </div>
   );

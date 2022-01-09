c9be55bf55af7b2cdb9bdf16f3f2918d1496a2f0
yurifws <yurifws@gmail.com>
2022-01-08 21:17:29 -0300

    Como funcionam as props dentro do React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Clock from './Clock';
-import Person from './Person';
+import Post from './components/Post';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
+  const [post, setPost] = useState({
+    title: 'Titulo maneiro',
+    content: 'Lorem ipsum dolot sit amet'
+  })
+
+//  setTimeout(() => {
+//    setPost({
+//      title: 'Titulo maneirissimo',
+//      content: 'Lorem ipsum dolot sit amet 2'
+//    })
+//
+//  }, 5000)
   return (
     <div className="App">
       <header className="App-header">
-        <Clock/>
-        <Person />
+        <Sidebar post={post}/>
+        <Post post={post} totalComents={12} />
         
       </header>
     </div>
---
+++ src/components/Post.tsx
@@ -0,0 +1,26 @@
+export type PostObject = {
+  title: string 
+  content: string
+}
+
+interface PostProps {
+  post: PostObject
+  totalComents?: number
+}
+
+export default function Post(props: PostProps) {
+  console.log(props)
+  console.log('Post foi renderizado')
+  return <div>
+    <h1>{props.post.title}</h1>
+    <p>{props.post.content}</p>
+
+    {
+      props.totalComents &&
+        <span>
+          Comentários: {props.totalComents}
+        </span>
+    }
+    
+  </div>
+}

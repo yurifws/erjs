e478d35838221dbf3eb8688af56f9b6a80185674
yurifws <yurifws@gmail.com>
2022-01-09 18:06:25 -0300

    Manipulação de eventos

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
-import React, { useState } from 'react';
-import './App.css';
-import Card from './components/Card';
-import Post, { PostObject } from './components/Post';
 
+import React from 'react';
+import './App.css';
+
+function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  e.preventDefault()
+}
 
 function App() {
-  console.log('renderizou app')
-  const [post, setPost] = useState<PostObject | undefined>({
-    title: 'Titulo maneiro',
-    content: 'Lorem ipsum dolot sit amet'
-  })
-
-
   return (
     <div className="App">
       <header className="App-header">
-        
-        {
-          !!post &&
-            <Card title={"Card"} align='left'>
-                <Post post={post} totalComents={12} />
-            </Card>
-        }
-        <button onClick={() => setPost(undefined)}>
-          Remover post
-        </button>
+
+        <form action="/test">
+          <button onClick={handleClick}>
+            Clique em mim
+          </button>
+
+        </form>
       </header>
     </div>
   );

531565b28db2788de8bfe73623cd4560ed38f33e
yurifws <yurifws@gmail.com>
2022-01-09 02:16:34 -0300

    Estados em Class Components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,33 @@ import { Component } from "react";
 
 interface ButtonProps {
   onClick?: () => any
+  initialazeClicked?: boolean
 }
 
-class Button extends Component<ButtonProps> {
+interface ButtonState {
+  alreadyClicked: boolean
+}
+
+class Button extends Component<ButtonProps, ButtonState> {
+
+  constructor(props: ButtonProps) {
+    super(props)
+
+    this.state = {
+      alreadyClicked: !!props.initialazeClicked
+    }
+  }
+
   render() {
-    return <button onClick={this.props.onClick}>
+    return <button 
+      onClick={() => {
+        this.setState({
+          alreadyClicked: true
+        })
+        this.props.onClick?.call([])
+      }}
+      disabled={this.state.alreadyClicked}
+      >
       {this.props.children}
     </button>
   }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,7 +14,9 @@ export default function Card(props: CardProps) {
     <C.Title>{props.title}</C.Title>
     {props.children}
     <div>
-      <Button onClick={() => console.log('batata')}>
+      <Button 
+        onClick={() => console.log('batata')}
+      >
         Ver mais
       </Button>
     </div>

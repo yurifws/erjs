f73e0c099c3a8f59641585ca21c1762b47f5f530
yurifws <yurifws@gmail.com>
2022-01-10 00:41:15 -0300

    Renderizando listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 
 import React from 'react';
 import './App.css';
-import Form from './components/Form';
 
 
 function App() {
+  const names = ['Daniel', 'Thiago', 'Alex', 'João']
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <ul>
+          {
+            names.map((name, index) => <li key={index}>{name}</li>)
+          }
+        </ul>
       </header>
     </div>
   );
